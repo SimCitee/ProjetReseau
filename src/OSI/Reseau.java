c229dd858a65e8e9276be22c34c5de9247218468
TiPhil <philip.cournoyer@live.ca>
2014-03-31 21:55:48 -0400

    Début de la couche réseau

--- src/OSI/Reseau.java
+++ src/OSI/Reseau.java
@@ -4,6 +4,10 @@ import java.io.PipedInputStream;
 import java.io.PipedOutputStream;
 import java.util.Random;
 
+import Paquet.Paquet;
+import Paquet.PaquetAppel;
+import Paquet.PaquetIndicationLiberation;
+
 /*
  * Singleton pour la couche réseau
  * 
@@ -92,7 +96,37 @@ public class Reseau  extends Thread{
 	//Les commande complètes provenant de la couche transport sont ici!!!
 	private void executerCommandeTransport(String command)
 	{
+		String[] commandArray = null;
+		Paquet paquet = null;
+		Paquet reponse = null;
+		commandArray = command.split(" ");
+		boolean versLiaison = true;
 		
+		// TODO
+		//SI NIEC est dans TABLE CORRESPONDANCE
+		// POGNER le # connexion correspondant
+		// SINON
+		// AJOUTER LE NIEC DANS LA TABLE et lui associer un numéro de connexion
+		
+		// 
+		
+		switch (commandArray[1]) {
+		case "N_CONNECT.req" : 
+			int temp = Integer.parseInt(commandArray[2]);
+			if (temp % 27 == 0) {
+				paquet = new PaquetAppel(1, Integer.parseInt(commandArray[2]), Integer.parseInt(commandArray[3]));
+			}
+			else {
+				versLiaison = false;
+				ecrireVersTransport(commandArray[0] + " N_DISCONNECT.ind " + commandArray[3] + " Refus de connexion");
+			} break;
+		case "N_DATA.req" : break;
+		case "N_DISCONNECT.req" : paquet = new PaquetIndicationLiberation(1, Integer.parseInt(commandArray[2]), Integer.parseInt(commandArray[3]), "Demande de transport"); break;
+		}
+		
+		// Si retour == null -> temporisateur
+		if (versLiaison == true)
+			reponse = Liaison.getInstance().lireDeReseau(paquet);
 		
 		System.out.println("Réseau recois une commande de transport : " + command);
 		
